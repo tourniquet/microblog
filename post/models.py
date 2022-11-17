bed3b7db0aad4d96ae72ad92eb08adbe02fc576a
tourniquet <admyn3d@gmail.com>
2022-11-17 22:20:26 +0000

    created models for Topic and Post

--- post/models.py
+++ post/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Topic(models.Model):
+  text = models.CharField(max_length=100)
+
+  def __str__(self):
+    return self.text
+
+
+class Post(models.Model):
+  topic = models.ForeignKey(Topic, on_delete=models.PROTECT)
+  text = models.TextField()
+  date = models.DateTimeField(auto_now_add=True)
+
+  class Meta:
+    verbose_name_plural = 'posts'
+
+  def __str__(self):
+    return f'{self.text[:50]}...'
