ace220e4effafeaa930dc2b798309a098ce446fe
tourniquet <admyn3d@gmail.com>
2022-11-28 21:40:38 +0000

    settings to serve static files like styles or images

--- post/urls.py
+++ post/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from django.conf.urls.static import static
+from django.conf import settings
 
 from . import views
 
@@ -6,4 +8,4 @@ app_name = 'post'
 urlpatterns = [
   path('', views.index, name='index'),
   path('post/<int:post_id>/', views.post, name='post'),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
